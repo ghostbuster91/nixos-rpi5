192e52a64a8a58553537ecf5b87a7ed6020e58e1
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2025-07-20 14:54:41 +0200

    Uncomment

--- machines/malina5/default.nix
+++ machines/malina5/default.nix
@@ -4,17 +4,17 @@
 
   imports =
     [
-            # inputs.nixos-raspberrypi.nixosModules.trusted-nix-caches
-            # inputs.nixos-raspberrypi.nixosModules.nixpkgs-rpi
+            inputs.nixos-raspberrypi.nixosModules.trusted-nix-caches
+            inputs.nixos-raspberrypi.nixosModules.nixpkgs-rpi
             inputs.nixos-raspberrypi.nixosModules.bootloader
-            # inputs.nixos-raspberrypi.nixosModules.default
-            # inputs.nixos-raspberrypi.lib.int.default-nixos-raspberrypi-config
-            # inputs.nixos-raspberrypi.nixosModules.raspberry-pi-5.base
-            # inputs.nixos-raspberrypi.nixosModules.raspberry-pi-5.display-vc4
-            # inputs.nixos-raspberrypi.nixosModules.raspberry-pi-5.bluetooth
+            inputs.nixos-raspberrypi.nixosModules.default
+            inputs.nixos-raspberrypi.lib.int.default-nixos-raspberrypi-config
+            inputs.nixos-raspberrypi.nixosModules.raspberry-pi-5.base
+            inputs.nixos-raspberrypi.nixosModules.raspberry-pi-5.display-vc4
+            inputs.nixos-raspberrypi.nixosModules.raspberry-pi-5.bluetooth
             ./configuration.nix
-            # ./monitoring.nix
-            # ./pi5-config.nix
+            ./monitoring.nix
+            ./pi5-config.nix
     ];
 
 }
